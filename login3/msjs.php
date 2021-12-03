36d31c990799be3ff4c47f41de61e50d0f32517d
URIAN VIERA <urian1213viera@gmail.com>
2021-12-03 09:42:20 -0500

    Update msjs.php

--- login3/msjs.php
+++ login3/msjs.php
@@ -3,7 +3,24 @@
 //Error de Login
 if(isset($_REQUEST['b'])){ ?>
 <script type='text/javascript'>
-	toastr.error('Error de Login, por favor verifique sus crendenciales.');
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+toastr.error('Error de Login, por favor verifique sus crendenciales.');
 </script>
 <?php } 
 //Sesion cerrada correctamente
