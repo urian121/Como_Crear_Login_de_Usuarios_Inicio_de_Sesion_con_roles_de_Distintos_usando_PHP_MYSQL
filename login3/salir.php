2ed01448fc0a47105737673931272f81f8e60dc5
URIAN VIERA <urian1213viera@gmail.com>
2021-12-03 09:02:14 -0500

    Update salir.php
    Mejorando el cierre de sesion

--- login3/salir.php
+++ login3/salir.php
@@ -1,9 +1,27 @@
 <?php
 session_start();
-unset ($_SESSION['id']);
+include('conexion/config.php');
+
+date_default_timezone_set("America/Bogota");
+$sesionHasta   = date("Y-m-d H:i:A");
+
+$Update = ("UPDATE myusers SET sesionHasta='$sesionHasta' WHERE IdUser='".$_SESSION["IdUser"]."' ");
+$resultado = mysqli_query($con, $Update);
+
+
+//Eliminando las  cookies  en session
+setcookie ($_SESSION['IdUser'], "", 1);
+setcookie ($_SESSION['IdUser'], false);
+unset($_COOKIE[$_SESSION['IdUser']]);
+
+unset ($_SESSION['IdUser']); // Eliminar el IdUser de usuario
+session_unset(); //Eliminar todas las sesiones
+
+// Terminar la sesión:
 session_destroy();
 $parametros_cookies = session_get_cookie_params();
 setcookie(session_name(),0,1,$parametros_cookies["path"]);
 
-header("Location: index.php?cc=1");
+
+header("Location: index.php?sc=1");
 ?>
