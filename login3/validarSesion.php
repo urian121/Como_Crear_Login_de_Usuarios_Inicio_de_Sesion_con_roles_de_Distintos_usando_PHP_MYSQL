6f9ef4b1beb3baea8c541d9b78c56a853a1aeed8
URIAN VIERA <urian1213viera@gmail.com>
2021-12-11 14:01:22 -0500

    Update validarSesion.php

--- login3/validarSesion.php
+++ login3/validarSesion.php
@@ -9,17 +9,16 @@ if(($_SERVER["REQUEST_METHOD"] == "POST")){
 		$passwordUser  = ($_POST["passwordUser"]); 
 
 		/*
-		El problema está en lo COLLATE que tiene en la base de datos latin1 para ese campo.
-		Tienes 2 opciones; el primero es realizar la consulta sin collate:
-		La segunda opción es modificar el COLLATE de esos campos:
 		https://bugs.mysql.com/bug.php?id=71939
-		ALTER TABLE users CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
-		*/
-		//ALTER DATABASE MyDataBase COLLATE utf8_bin
 		
-		//$sqlVerificando = ("SELECT * FROM myusers WHERE emailUser COLLATE utf8_bin='".$emailUser."' AND passwordUser COLLATE utf8_bin='".$passwordUser."' ");
-		//Evitar los Select * From xyz
-		//No se debe declarar la session hasta saber si los datos son correctos
+		ALTER TABLE users CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+		ALTER DATABASE MyDataBase COLLATE utf8_bin
+		
+		$sqlVerificando = ("SELECT * FROM myusers WHERE emailUser COLLATE utf8_bin='".$emailUser."' AND passwordUser COLLATE utf8_bin='".$passwordUser."' ");
+		Evitar los Select * From xyz
+		No se debe declarar la session hasta saber si los datos son correctos
+		*/
+
 		$sqlVerificandoLogin = ("SELECT IdUser, nameUser, emailUser, passwordUser  FROM myusers WHERE emailUser COLLATE utf8_bin='$emailUser'");
 		$resultLogin = mysqli_query($con, $sqlVerificandoLogin) or die(mysqli_error($con));;
 		$numLogin    = mysqli_num_rows($resultLogin);
@@ -29,10 +28,9 @@ if(($_SERVER["REQUEST_METHOD"] == "POST")){
 			while($rowData  = mysqli_fetch_assoc($resultLogin)){
 				$passwordBD = $rowData['passwordUser'];
 				//password_verify — Comprueba que la contraseña facilitada coincida con un hash, 
-				//retorna una respuesta de tipo booleano es decir 1 - 0 TRUE -FALSE
+				//retorna una respuesta de tipo booleano es decir (1 - 0) (TRUE - FALSE)
 				/* Ademas es capaz de encontrar la correspondencia entre 
-				cualquiera de los múltiples hash que puede generar password_hash (recuerde que cada vez que se ejecuta,
-					aún a partir de la misma contraseña, genera uno diferente) con la contraseña que se le suministre.*/
+				cualquiera de los múltiples hash que puede generar password_hash (recuerde que cada vez que se ejecuta,aún a partir de la misma contraseña, genera uno diferente) con la contraseña que se le suministre.*/
 				if(password_verify($passwordUser, $passwordBD)) {
 					session_start(); //Creando la sesion ya que los datos son validos
 					$_SESSION['IdUser'] 	= $rowData['IdUser']; 
@@ -56,25 +54,5 @@ if(($_SERVER["REQUEST_METHOD"] == "POST")){
 			header("location:./?e=1");
 		}
 	}
-
-/*
-$passwordCandidate = '123'; 
-$passwordHash = password_hash($passwordCandidate, PASSWORD_DEFAULT);
-if (password_Verify($passwordCandidate, $passwordHash)) {
-	echo 'Password correcta';
-} else {
-	echo 'Password Incorrecta!';
-}
-
-//  
-$tesLogin = password_verify($passwordCandidate, $passwordHash);
- if($tesLogin == true) {
-	echo "VALID password for the informed HASH!<br>"; 
-	var_dump($tesLogin);
- } else {
-	echo "INVALID password for the informed HASH!<br>";     
-	var_dump($tesLogin);    
- }
-*/
-
+	
 ?>
